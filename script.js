f1410e3a917e93e7e55864c7613c6500af6673b7
TimLachner <66732131+TimLachner@users.noreply.github.com>
2020-06-25 12:15:05 +0200

    .

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ const displayPokemon = (pokemon) => {
             <img class="card-image" src="${pokemon.image}"/>
             <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
             <p class="card-subtitle">Type: ${pokemon.type}</p>
+
         </li>
     `
     )
@@ -40,21 +41,4 @@ el.addEventListener("input", logkey);
 function logkey(e) {
   var input = document.getElementById("suchtext").value;
   console.log(input);
-}
-
-function mySearchFunction() {
-  var input, filter, ul, li, item, i, txtValue;
-  input = document.getElementById(input);
-  filter = input.value.toUpperCase();
-  ul = document.getElementsByClassName("card-title");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-    item = li[i];
-    txtValue = item.textContent || item.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
 }
